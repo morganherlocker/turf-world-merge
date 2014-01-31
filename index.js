3025779fd6f1b79b106edb4d46e6eb15bb56738f
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-01-30 19:16:36 -0500

    all working with new polys

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ t.load('./countries.geojson', function(err, countries){
   // merge the feature collection
   t.merge(countries, function(err, world){
     // write out the file
-    fs.writeFile('./world.geojson', world, function(err){
+    fs.writeFile('./world.geojson', JSON.stringify(world), function(err){
       console.log('presto!')
     })
   })
